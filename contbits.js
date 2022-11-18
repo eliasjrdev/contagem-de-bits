29ab7f9d329ea56d83246081521b6d72a9b69f89
unknown <eliasjrcosta886@gmail.com>
2022-11-18 15:21:32 -0300

    trabalhando na extração de strings

--- contbits.js
+++ contbits.js
@@ -7,13 +7,20 @@ botao1.addEventListener('click', rodar);
 botao2.addEventListener('click', apagar);
 
 function rodar(){
-    let v3 = parseInt(v1);
-    let bin = v3.toString(2);
-    resposta.style.display = 'flex';
-    resposta.innerHTML = bin.value
+    let deposito = 0
+    let v3 = v1.value;
+    let convert = (v3 >>> 0).toString(2)
+    resposta.style.display = 'block';
+    resposta.innerHTML = convert;
+    let tam = convert.length
+    for (var test = tam; tam === 1; deposito++ ){
+        console.log(test)
+    }
 }
 
 function apagar(){
     v1.value = '';
     resposta.innerHTML = '';
+    resposta.style.display = 'none';
 }
+
